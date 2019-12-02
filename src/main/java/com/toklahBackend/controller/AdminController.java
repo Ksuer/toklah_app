6299ea4034b2fda5e75fa66030bed727c29830b7
Kuser <almaari.eman@gmail.com>
2019-12-02 11:40:04 +0300

    adding new admin services

--- src/main/java/com/toklahBackend/controller/AdminController.java
+++ src/main/java/com/toklahBackend/controller/AdminController.java
@@ -1,18 +1,28 @@
 package com.toklahBackend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.toklahBackend.exception.NotFoundException;
 import com.toklahBackend.model.Admin;
+import com.toklahBackend.model.ChangePassword;
 import com.toklahBackend.model.Login;
+import com.toklahBackend.model.Ticket;
+import com.toklahBackend.model.User;
 import com.toklahBackend.service.Imp.AdminServiceImp;
 
 
@@ -46,6 +56,51 @@ public class AdminController {
 			return new ResponseEntity<>(admin,HttpStatus.OK);
 		}
 		
+	}
+	
+	/*@RequestMapping(value = "/getprofile/{adminId}", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<?> getadmin(@PathVariable int adminId) {
+		Admin admin = adminServiceImp.getAdmin(adminId);
+		if (admin == null) {
+			return new ResponseEntity<>("admin not found", HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(adminServiceImp.getAdmin(adminId), HttpStatus.OK);
+		}
+	}*/
+	
+	@RequestMapping(value = "/getprofile", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<?> getadmin(@RequestHeader (name="Authorization") String token) {
+		Admin admin = adminServiceImp.getAdmin(token);
+		if (admin == null) {
+			return new ResponseEntity<>("admin not found", HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(adminServiceImp.getAdmin(token), HttpStatus.OK);
+		}
+	}
+	
+	@RequestMapping(value = "/{eventId}/acceptEventRequest", method = RequestMethod.PUT)
+	@ResponseBody
+	public ResponseEntity<?> accepteventrequest(@PathVariable int eventId) {
+		//return new ResponseEntity<>(adminServiceImp.acceptEventRequest(eventId), HttpStatus.OK);
+		adminServiceImp.accepteventrequest(eventId);
+		return new ResponseEntity<>("event request accepted", HttpStatus.OK);
+
+	}
+	
+	@RequestMapping(value = "/{adminId}/editAdmin", method = RequestMethod.PUT)
+	@ResponseBody
+	public ResponseEntity<?> editaccount(@PathVariable int adminId, @RequestBody Admin admin) throws Exception {
+		return new ResponseEntity<>(adminServiceImp.editAdmin(adminId, admin), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/changepassword", method = RequestMethod.PUT)
+	@ResponseBody
+	public ResponseEntity<?> changepassword(@RequestBody ChangePassword changePassword) {
+		adminServiceImp.changePassword(changePassword.getOldPassword(), changePassword.getNewPassword(),
+				changePassword.getAdminId());
+		return new ResponseEntity<>("password changed successfully", HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/toklahBackend/service/AdminService.java
+++ src/main/java/com/toklahBackend/service/AdminService.java
@@ -1,7 +1,14 @@
 package com.toklahBackend.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.toklahBackend.model.Admin;
 import com.toklahBackend.model.Login;
+import com.toklahBackend.model.Ticket;
+import com.toklahBackend.model.User;
 
 import javassist.NotFoundException;
 
@@ -12,5 +19,15 @@ public interface AdminService {
 	Admin login(Login login) throws NotFoundException, Exception;
 
 	Admin addAdmins(Admin admin, int priority) throws Exception;
+
+	//Admin getAdmin(int adminId);
 	
+	Admin getAdmin(String token);
+
+	void accepteventrequest(int eventId);
+
+	Admin editAdmin(int adminId, Admin admin);
+
+	void changePassword(String oldPassword, String newPassword, int adminId);
+
 }
